eee29b577a5404012d5f77555e74f4aecda03e07
MikeMurray13 <>
2023-10-20 00:36:56 +0000

    Added rules to disable buttons to prevent player leaving roomMatrix

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,18 @@
 const roomMatrix = [
     [
-        /*0.0*/ { heading: "Room 1", description: "Room 1" },
-        /*0.1*/ { heading: "Room 2", description: "Room 2", choices: "<button onclick =\"move('south')\">SOUTH</button><button onclick='addInventory(\"sword\")'>SWORD</button>" },
-        /*0.2*/ { heading: "Room 3", description: "Room 3" }
+        /*0.0*/ { heading: "Room 1", description: "Room 1", choices: [] },
+        /*0.1*/ { heading: "Room 2", description: "Room 2", choices: ["<button onclick='addInventory(\"sword\")'>SWORD</button>"] },
+        /*0.2*/ { heading: "Room 3", description: "Room 3", choices: [] }
     ],
     [
-        /*1.0*/ { heading: "Room 4", description: "Room 4", choices: "<button onclick =\"move('east')\">EAST</button>" },
-        /*1.1*/ { heading: "Room 5", description: "The room is dark.", choices: "<button onclick =\"move('north')\">NORTH</button><button onclick =\"move('south')\">SOUTH</button><button onclick =\"move('west')\">WEST</button><button onclick =\"move('east')\">EAST</button>"},
-        /*1.2*/ { heading: "Room 6", description: "Room 6", choices: "<button onclick =\"move('west')\">WEST</button>" }
+        /*1.0*/ { heading: "Room 4", description: "Room 4", choices: [] },
+        /*1.1*/ { heading: "Room 5", description: "The room is dark.", choices: [] },
+        /*1.2*/ { heading: "Room 6", description: "Room 6", choices: [] }
     ],
     [
-        /*2.0*/ { heading: "Room 7", description: "Room 7" },
-        /*2.1*/ { heading: "Room 8", description: "Room 8", choices: "<button onclick =\"move('north')\">NORTH</button><button onclick='lightSwitch()'>LIGHT SWITCH</button>" },
-        /*2.2*/ { heading: "Room 9", description: "Room 9" }
+        /*2.0*/ { heading: "Room 7", description: "Room 7", choices: [] },
+        /*2.1*/ {},
+        /*2.2*/ { heading: "Room 9", description: "Room 9", choices: ["<button onclick='lightSwitch()'>LIGHT SWITCH</button>"] }
     ]
 ];
 
@@ -20,7 +20,7 @@ let gameState = {
     //Game starts with starting room declared as the current room
     currX: 1,
     currY: 1
-}
+};
 
 let inventory = [];
 
@@ -28,43 +28,63 @@ function startGame() {
     //Hide startScreen and displays the main game content box   
     document.getElementById("startScreen").style = "display: none";
     document.getElementById("content").style = "display: block";
+
     updateContent();
-    //Test method using console
-    console.log(roomMatrix[gameState.currX][gameState.currY].description);
-    console.log(gameState.currX);
-    console.log(gameState.currY);
 }
 
 //Basic move function
 function move(dir) {
-    if (dir === 'north') {gameState.currX -= 1};
-    if (dir === 'south') { gameState.currX += 1;};
-    if (dir === 'west') { gameState.currY -= 1; };
-    if (dir === 'east') { gameState.currY += 1; };
+    if (dir == 'north') { gameState.currX -= 1; };
+    if (dir == 'south') { gameState.currX += 1 };
+    if (dir == 'west') { gameState.currY -= 1 };
+    if (dir == 'east') { gameState.currY += 1 };
+
+    //Updates the innerHTML of all fields to that of new page
     updateContent();
 }
 
 //Displays the values provided by the current room
 function updateContent() {
-document.getElementById("headingBox").innerHTML = roomMatrix[gameState.currX][gameState.currY].heading;
-document.getElementById("textBox").innerHTML = roomMatrix[gameState.currX][gameState.currY].description;
-document.getElementById("choiceBox").innerHTML = roomMatrix[gameState.currX][gameState.currY].choices;
+
+    let X = gameState.currX;
+    let Y = gameState.currY;
+
+    //updates values of each room
+    document.getElementById('headingBox').innerHTML = roomMatrix[X][Y].heading;
+    document.getElementById('textBox').innerHTML = roomMatrix[X][Y].description;
+    document.getElementById('choiceBox').innerHTML = roomMatrix[X][Y].choices;
+
+    //Disables buttons if risk of leaving roomMatrix
+    if (X == 0) { document.getElementById("northButton").disabled = true; } else { document.getElementById("northButton").disabled = false };
+    if (X == (roomMatrix.length - 1)) { document.getElementById("southButton").disabled = true; } else { document.getElementById("southButton").disabled = false; };
+    if (Y == 0) { document.getElementById("westButton").disabled = true; } else { document.getElementById("westButton").disabled = false; };
+    if (Y == (roomMatrix[X].length - 1)) { document.getElementById("eastButton").disabled = true; } else { document.getElementById("eastButton").disabled = false; };
+
+
+    //Disables buttons that lead to empty rooms in roomMatrix
+  //  if (typeof roomMatrix[X-1][Y] !== "object") { document.getElementById("northButton").disabled = true; } else { document.getElementById("northButton").disabled = false; };
+//if (roomMatrix[X+1][Y].length == 0) { document.getElementById("southButton").disabled = true; } else { document.getElementById("southButton").disabled = false; };
+ //   if (typeof roomMatrix[X][Y-1] !== "object") { document.getElementById("westButton").disabled = true; } else { document.getElementById("westButton").disabled = false; };
+ //   if (typeof roomMatrix[X][Y+1] !== "object") { document.getElementById("eastButton").disabled = true; } else { document.getElementById("eastButton").disabled = false; };
+   
 }
 
 
 //Creates an alert when the player tries to leave the page
+/*
 function leavePageAlert() {
-    return "Write something clever here...";
+    return "Leaving will cause you to lose any unsaved progress...";
 }
+*/
 
 //Turns the light on in Room 5
 function lightSwitch() {
-    roomMatrix[1][1].description = 'The room is well lit.'; 
-    console.log(roomMatrix[1][1].description);
+    roomMatrix[1][1].description = 'The room is well lit.';
 }
 
 
 function addInventory(item) {
-    inventory.push({item});
+    inventory.push({ item });
     console.log(inventory);
 }
+
